12be2b6dc5b21d2153ef832061eaab6979e26686
Keo <keonilrath@gmail.com>
2016-09-26 13:39:39 -0400

    comments added to improve ai

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,20 @@ module Players
   class Computer < Player
 
     attr_reader :move
-
+#todo
+#board = ["X", "", "", "", "", "", "", "",""]
+#board = ["X", "O", "", "", "X", "", "", "",""] 
+#board = ["X", "O", "", "", "X", "", "", "",""]
+#board = ["X", "O", "", "", "X", "", "", "","O"]
+#board = ["X", "O", "X", "", "X", "", "", "","O"]
+#board = ["X", "O", "X", "", "X", "", "O", "","O"]
+#board = ["X", "O", "X", "", "X", "X", "O", "","O"]
+#board = ["X", "O", "X", "O", "X", "X", "O", "","O"]
+#board = ["X", "O", "X", "O", "X", "X", "O", "","O"]
+#board = ["X", "O", "X", "O", "X", "X", "O", "X","O"]
+#calculate draw combos, iterate though and base ai over moves on it 
+    
+#dumdum AI will lose more often than not, CLI often breaks if game_type == ai_vs_ai
     def move(board)
       if (board.cells[0] || board.cells[2] || board.cells[4] || board.cells[6] || board.cells[8]) == "X"
         ai_move = ["2", "4", "6", "8"]
