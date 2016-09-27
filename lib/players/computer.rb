db8c9f78b7d3aaee24ac57fd6a278ab24d8b9448
Keo  Bun <keonilrath@gmail.com>
2016-09-27 01:59:47 +0000

    AI no longer seems to crash Atom, but AI still dum

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,57 +15,59 @@ module Players
 #board = ["X", "O", "X", "O", "X", "X", "O", "X","O"]
 #calculate draw combos as a class constant, iterate though and base ai over moves on it, will the AI refuse to win in an Ai vs Human game in this case?
 #If not, maybe this should be implemented.
-#would be interesting to see two opposing dumdums racing to bottom in a more multi-faceted game,
+#would be interesting to see two opposing dumdums racing to the bottom in a more multi-faceted game,
 #like Lemmings the game meets Iodiocracy the movie
 
+
+#very dumb AI
     def move(board)
-      if @players == "X" && board.cells[0] == "X" && board.cells[1] == "O"|| board.cells[3] == "O"|| board.cells[4] == "O"
+      if @players == "X" && board.cells[0] == "X" && (board.cells[1] == "O"|| board.cells[3] == "O"|| board.cells[4] == "O")
         ai_move = ["3", "5", "6", "8"]
         ai_move.sample
-      elsif @players == "X" && board.cells[1] == "X" && board.cells[0] == "O"|| board.cells[2] == "O"|| board.cells[3] == "O" || board.cells[4] == "O" || board.cells[5] == "O"
+      elsif @players == "X" && board.cells[1] == "X" && (board.cells[0] == "O"|| board.cells[2] == "O"|| board.cells[3] == "O" || board.cells[4] == "O" || board.cells[5] == "O")
         ai_move = ["4", "5", "6", "8"]
         ai_move.sample
-      elsif @players == "X" && board.cells[2] == "X" && board.cells[1] == "O"|| board.cells[4] == "O"|| board.cells[5] == "O"
+      elsif @players == "X" && board.cells[2] == "X" && (board.cells[1] == "O"|| board.cells[4] == "O"|| board.cells[5] == "O")
         ai_move = ["1", "5", "7", "9"]
         ai_move.sample
-      elsif @players == "X" && board.cells[3] == "X" && board.cells[0] == "O"|| board.cells[1] == "O"|| board.cells[4] == "O" || board.cells[6] == "O" || board.cells[7] == "O"
+      elsif @players == "X" && board.cells[3] == "X" && (board.cells[0] == "O"|| board.cells[1] == "O"|| board.cells[4] == "O" || board.cells[6] == "O" || board.cells[7] == "O")
         ai_move = ["4", "5", "6", "8"]
         ai_move.sample
-      elsif @players == "X" && board.cells[4] == "X" && board.cells[0] == "O"|| board.cells[1] == "O"|| board.cells[2] == "O" || board.cells[3] == "O" || board.cells[5] == "O" || board.cells[6] == "O" || board.cells[7] == "O" || board.cells[8] == "O"
+      elsif @players == "X" && board.cells[4] == "X" && (board.cells[0] == "O"|| board.cells[1] == "O"|| board.cells[2] == "O" || board.cells[3] == "O" || board.cells[5] == "O" || board.cells[6] == "O" || board.cells[7] == "O" || board.cells[8] == "O")
         ai_move = ["1", "3", "7", "9"]
         ai_move.sample
-      elsif @players == "X" && board.cells[5] == "X" && board.cells[1] == "O"|| board.cells[2] == "O"|| board.cells[4] == "O" || board.cells[7] == "O" || board.cells[8] == "O"
+      elsif @players == "X" && board.cells[5] == "X" && (board.cells[1] == "O"|| board.cells[2] == "O"|| board.cells[4] == "O" || board.cells[7] == "O" || board.cells[8] == "O")
         ai_move = ["3", "4", "5", "9"]
         ai_move.sample
-      elsif @players == "X" && board.cells[6] == "X" && board.cells[3] == "O"|| board.cells[4] == "O"|| board.cells[7] == "O"
+      elsif @players == "X" && board.cells[6] == "X" && (board.cells[3] == "O"|| board.cells[4] == "O"|| board.cells[7] == "O")
         ai_move = ["1", "3", "5", "9"]
         ai_move.sample
-      elsif @players == "X" && board.cells[7] == "X" && board.cells[3] == "O"|| board.cells[4] == "O"|| board.cells[5] == "O" || board.cells[6] == "O" || board.cells[8] == "O"
+      elsif @players == "X" && board.cells[7] == "X" && (board.cells[3] == "O"|| board.cells[4] == "O"|| board.cells[5] == "O" || board.cells[6] == "O" || board.cells[8] == "O")
         ai_move = ["2", "5", "7", "9"]
         ai_move.sample
-      elsif @players == "X" && board.cells[8] == "X" && board.cells[4] == "O"|| board.cells[5] == "O"|| board.cells[7] == "O"
+      elsif @players == "X" && board.cells[8] == "X" && (board.cells[4] == "O"|| board.cells[5] == "O"|| board.cells[7] == "O")
         ai_move = ["1", "3", "5", "6"]
         ai_move.sample
 
-      elsif @players == "O" && board.cells[0] == "O" && board.cells[1] == "X"|| board.cells[3] == "O"|| board.cells[4] == "O"
+      elsif @players == "O" && board.cells[0] == "O" && (board.cells[1] == "X"|| board.cells[3] == "O"|| board.cells[4] == "O")
         ai_move = ["3", "5", "6", "8"]
         ai_move.sample
-      elsif @players == "O" && board.cells[1] == "O" && board.cells[0] == "X"|| board.cells[2] == "X"|| board.cells[3] == "X" || board.cells[4] == "X" || board.cells[5] == "X"
+      elsif @players == "O" && board.cells[1] == "O" && (board.cells[0] == "X"|| board.cells[2] == "X"|| board.cells[3] == "X" || board.cells[4] == "X" || board.cells[5] == "X")
         ai_move = ["4", "5", "6", "8"]
         ai_move.sample
-      elsif @players == "O" && board.cells[2] == "O" && board.cells[1] == "X"|| board.cells[4] == "X"|| board.cells[5] == "X"
+      elsif @players == "O" && board.cells[2] == "O" && (board.cells[1] == "X"|| board.cells[4] == "X"|| board.cells[5] == "X")
         ai_move = ["1", "5", "7", "9"]
         ai_move.sample
-      elsif @players == "O" && board.cells[3] == "O" && board.cells[0] == "X"|| board.cells[1] == "X"|| board.cells[4] == "X" || board.cells[6] == "X" || board.cells[7] == "X"
+      elsif @players == "O" && board.cells[3] == "O" && (board.cells[0] == "X"|| board.cells[1] == "X"|| board.cells[4] == "X" || board.cells[6] == "X" || board.cells[7] == "X")
         ai_move = ["4", "5", "6", "8"]
         ai_move.sample
-      elsif @players == "O" && board.cells[4] == "O" && board.cells[0] == "X"|| board.cells[1] == "X"|| board.cells[2] == "X" || board.cells[3] == "X" || board.cells[5] == "X" || board.cells[6] == "X" || board.cells[7] == "X" || board.cells[8] == "X"
+      elsif @players == "O" && board.cells[4] == "O" && (board.cells[0] == "X"|| board.cells[1] == "X"|| board.cells[2] == "X" || board.cells[3] == "X" || board.cells[5] == "X" || board.cells[6] == "X" || board.cells[7] == "X" || board.cells[8] == "X")
         ai_move = ["1", "3", "7", "9"]
         ai_move.sample
-      elsif @players == "O" && board.cells[5] == "O" && board.cells[1] == "X"|| board.cells[2] == "X"|| board.cells[4] == "X" || board.cells[7] == "X" || board.cells[8] == "X"
+      elsif @players == "O" && board.cells[5] == "O" && (board.cells[1] == "X"|| board.cells[2] == "X"|| board.cells[4] == "X" || board.cells[7] == "X" || board.cells[8] == "X")
         ai_move = ["3", "4", "5", "9"]
         ai_move.sample
-      elsif @players == "O" && board.cells[6] == "O" && board.cells[3] == "X"|| board.cells[4] == "X"|| board.cells[7] == "X"
+      elsif @players == "O" && board.cells[6] == "O" && (board.cells[3] == "X"|| board.cells[4] == "X"|| board.cells[7] == "X")
         ai_move = ["1", "3", "5", "9"]
         ai_move.sample
       elsif @players == "O" && board.cells[7] == "O" && (board.cells[3] == "X"|| board.cells[4] == "X"|| board.cells[5] == "X" || board.cells[6] == "X" || board.cells[8] == "X")
